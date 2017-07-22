762e25471cec7ee777483a6004796958166ebb76
davidcurto <david.curto.c@gmail.com>
2017-07-22 08:57:22 -0700

    Update repository

---
+++ src/local_path_gazebo.cpp
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+#include <ros/param.h>
+#include <nav_msgs/Path.h>
+#include <tf/tf.h>
+
+// DCC - Local Path from Time Elastic Band
+
+int main(int argc, char** argv)
+{
+
+  ros::init(argc, argv, "local_path_publisher");
+  ros::NodeHandle nh;
+  std::string global_plan;
+
+  nh.param<std::string>("publish_topic", global_plan, "goal_path");
+
+  ros::Publisher pub_plan = nh.advertise<nav_msgs::Path>("/global_plan", 1);
+
+  geometry_msgs::PoseStamped point;
+  nav_msgs::Path path;
+
+  float radius = 10;
+  path.header.stamp = ros::Time::now();
+  path.header.frame_id = "base_footprint";
+  for(int i=0; i < 18 ; i++)
+  {
+    point.pose.position.x = radius * sin(20*M_PI/180 * i);
+    point.pose.position.y = -radius * cos(20*M_PI/180 * i) + radius;
+    point.pose.orientation = tf::createQuaternionMsgFromYaw(20*M_PI/180 * i);
+    path.poses.push_back(point);
+  }
+    pub_plan.publish(path);
+    sleep(1);
+    pub_plan.publish(path);
+    ros::spin();
+    return 0;
+}
