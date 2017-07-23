538c1e7d36fd64ad4d2eb81ccda80a140612d488
davidcurto <david.curto.c@gmail.com>
2017-07-23 22:00:39 +0200

    Next back up

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,27 @@
 #include <ros/ros.h>
-//#include <ros/param.h>
 #include <nav_msgs/Path.h>
-
+#include <nav_msgs/Odometry.h>
 
 // DCC - Main Function
 
 void mainfuntioncallback(const nav_msgs::Path::ConstPtr& msg){
-//void mainfuntioncallback(){
-  ROS_INFO("main_funtioncallback");
-  ROS_INFO("first x is: %f", msg->poses[0].pose.position.x);
-  ROS_INFO("first y is: %f", msg->poses[0].pose.position.y);
-  ROS_INFO("first angle is: %f", msg->poses[0].pose.orientation);
+  ROS_INFO("mainfuntioncallback");
+  //ROS_INFO("first x is: %f", msg->poses[0].pose.position.x);
+  //ROS_INFO("first y is: %f", msg->poses[0].pose.position.y);
+  //ROS_INFO("first angle is: %f", msg->poses[0].pose.orientation);
+  //for(int i=0; i < 18 ; i++){
+      //ROS_INFO("Position %i, Pose x = %f", i, msg->poses[i].pose.position.x);
+  //}
+}
+
+
+
+void odometrycallback(const nav_msgs::Odometry::ConstPtr& msg_odom){
+  ROS_INFO("odometrycallback");
+  ROS_INFO("odom || x = %f, y = %f, angle = %f",
+           msg_odom->pose.pose.position.x,
+           msg_odom->pose.pose.position.y,
+           msg_odom->pose.pose.orientation.z);
 }
 
 
@@ -21,10 +32,16 @@ int main(int argc, char** argv)
   ros::NodeHandle main_node_path;
   ROS_INFO("main node created and registered");
 
+  // Subscriber to Local Path
   ros::Subscriber main_subscriber = main_node_path.subscribe("local_path_publisher", 0, mainfuntioncallback);
-
   ROS_INFO("main_subscriber completed");
 
+  // Subscriber to Odometry
+  ros::Subscriber odometry_subscriber = main_node_path.subscribe("gazebo_odometry", 0, odometrycallback);
+  ROS_INFO("odometry_subscriber completed");
+
+
+  // Loop
   ros::spin();
 
   return 0;
