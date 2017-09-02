630bbb8004eafde521cbb987b8ac4d0e7a9e892e
davidcurto <david.curto.c@gmail.com>
2017-09-02 12:19:53 +0200

    added launch file

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,31 @@
 #include <nav_msgs/Odometry.h>
 #include <ackermann_msgs/AckermannDrive.h>
 
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+//#include <robotnik_pp_msgs/GoToAction.h>
+
+//#include <robotnik_pp_msgs/GoToActionGoal.h>
+//#include <robotnik_pp_msgs/GoToActionFeedback.h>
+//#include <robotnik_pp_msgs/GoToActionResult.h>
+
+//#include <robotnik_pp_msgs/GoToGoal.h>
+//#include <robotnik_pp_msgs/GoToFeedback.h>
+//#include <robotnik_pp_msgs/GoToResult.h>
+
+
 #include <iostream>
 
 using namespace std;
 
 // DCC - Main Function
 
+//http://wiki.ros.org/navigation/Tutorials/SendingSimpleGoals
+//https://github.com/RobotnikAutomation/robotnik_purepursuit_planner.git
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+//typedef robotnik_pp_msgs::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 ackermann_msgs::AckermannDrive acker_drive_msg;
 
